81888ff06f282cab55e6542069e971efb9e3295b
chethan.hanu <chethan.hanu@mfilterit.com>
2020-06-19 12:09:44 +0530

    Updates

---
+++ precache-manifest.3a8c838a67533ed706a146c33b5db92f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8fd7e91fbd02cc2255035f3a8318ea",
+    "url": "/indian-heros/index.html"
+  },
+  {
+    "revision": "255ac63124314e8f4b01",
+    "url": "/indian-heros/static/css/main.a279a963.chunk.css"
+  },
+  {
+    "revision": "e40d7a6e988f544e0e5a",
+    "url": "/indian-heros/static/js/2.58bd2db1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/indian-heros/static/js/2.58bd2db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255ac63124314e8f4b01",
+    "url": "/indian-heros/static/js/main.778681d5.chunk.js"
+  },
+  {
+    "revision": "19d77f1a067b85d60808",
+    "url": "/indian-heros/static/js/runtime-main.4cad4e98.js"
+  },
+  {
+    "revision": "7693a470d7eb130af76715ef2a637f1b",
+    "url": "/indian-heros/static/media/alphabets.7693a470.png"
+  },
+  {
+    "revision": "820ac60b481dee9148d6880adbef63ce",
+    "url": "/indian-heros/static/media/background.820ac60b.jpg"
+  },
+  {
+    "revision": "98d9d23bc48904c480a59aa0c8e399f2",
+    "url": "/indian-heros/static/media/facebook.98d9d23b.png"
+  },
+  {
+    "revision": "5ab0c78c8d0eed3769936940e586cfa5",
+    "url": "/indian-heros/static/media/instagram.5ab0c78c.png"
+  },
+  {
+    "revision": "de38ce245eec78fb702a5c08994ccb53",
+    "url": "/indian-heros/static/media/linkedin.de38ce24.png"
+  },
+  {
+    "revision": "a6f66336a098977e8cd0ca2db8acc513",
+    "url": "/indian-heros/static/media/logo.a6f66336.png"
+  },
+  {
+    "revision": "64452ef490ba4a5aed2bc37fa447ff3e",
+    "url": "/indian-heros/static/media/twitter.64452ef4.png"
+  }
+]);
